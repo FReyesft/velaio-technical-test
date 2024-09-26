15fef5991f30de93259acbca3c234d6262dda710
freyesft <f.reyesft@gmail.com>
2024-09-26 18:04:36 -0500

    [ADD] create task form, validators and add tasks service

--- frontend/src/app/pages/tasks/components/create-tasks/create-tasks.component.ts
+++ frontend/src/app/pages/tasks/components/create-tasks/create-tasks.component.ts
@@ -1,28 +1,24 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-
-export interface Person {
-  name: string,
-  age: number,
-  skills: Skill[]
-}
-
-export interface Skill {
-  name: string
-}
+import { MatChipEditedEvent, MatChipInputEvent } from '@angular/material/chips';
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Person } from 'src/app/interfaces/person.interface';
+import { Skill } from 'src/app/interfaces/skill.interface';
+import { MatSnackBar } from '@angular/material/snack-bar';
 
 @Component({
   selector: 'app-create-tasks',
   templateUrl: './create-tasks.component.html',
   styleUrls: ['./create-tasks.component.scss'],
 })
-
 export class CreateTasksComponent implements OnInit {
   public taskForm: FormGroup;
+  public skills: Skill[] = [];
+  public addOnBlur = true;
+  public readonly separatorKeysCodes = [ENTER, COMMA] as const;
+  public persons: Person[] = [];
 
-  constructor(
-    private fb: FormBuilder
-  ) {}
+  constructor(private fb: FormBuilder, private snackBar: MatSnackBar) {}
 
   ngOnInit(): void {
     this.initTaskForm();
@@ -30,17 +26,132 @@ export class CreateTasksComponent implements OnInit {
 
   initTaskForm() {
     this.taskForm = this.fb.group({
-      personName: ['', Validators.required],
+      taskName: ['', Validators.required],
       taskLimitDate: ['', Validators.required],
       taskAssociatedPersons: this.fb.group({
-        name: ['', Validators.required, Validators.minLength(5)],
-        age: ['', Validators.required, Validators.min(18)],
-        skills: [[], Validators.required, Validators.minLength(1)]
+        personName: ['', [Validators.required, Validators.minLength(5)]],
+        personAge: ['', [Validators.required, Validators.min(18)]],
       }),
     });
   }
 
   submitTask() {
-    console.log(this.taskForm)
+    this.removeValidators();
+    if (!this.taskForm.valid) {
+      this.addValidators();
+      return;
+    } else {
+      this.addValidators();
+      this.openSnackBar('La tarea se creo correctamente', '');
+    }
+    console.log(this.taskForm);
+  }
+
+  removeValidators() {
+    this.taskForm.get('taskAssociatedPersons.personName').clearValidators();
+    this.taskForm.get('taskAssociatedPersons.personAge').clearValidators();
+
+    this.taskForm.get('taskAssociatedPersons.personName').updateValueAndValidity();
+    this.taskForm.get('taskAssociatedPersons.personAge').updateValueAndValidity();
+  }
+
+  addValidators() {
+    this.taskForm.get('taskAssociatedPersons.personName').addValidators([Validators.required, Validators.minLength(5)]);
+    this.taskForm.get('taskAssociatedPersons.personAge').addValidators([Validators.required, Validators.min(18)]);
+  }
+
+  addSkill(event: MatChipInputEvent): void {
+    const value = (event.value || '').trim();
+
+    if (value) {
+      this.skills.push({ name: value });
+    }
+
+    event.chipInput!.clear();
+  }
+
+  removeSkill(skill: Skill): void {
+    const index = this.skills.indexOf(skill);
+
+    if (index >= 0) {
+      this.skills.splice(index, 1);
+    }
+  }
+
+  editSkill(skill: Skill, event: MatChipEditedEvent) {
+    const value = event.value.trim();
+
+    if (!value) {
+      this.removeSkill(skill);
+      return;
+    }
+
+    const index = this.skills.indexOf(skill);
+    if (index >= 0) {
+      this.skills[index].name = value;
+    }
+  }
+
+  isValidToAddPersons(): boolean {
+    const name: string = this.taskForm.get(
+      'taskAssociatedPersons.personName'
+    ).value;
+    const age: number = Number(
+      this.taskForm.get('taskAssociatedPersons.personAge').value
+    );
+    const isPersonExist = this.persons.some(
+      (person) => person?.name?.toLowerCase() === name?.toLowerCase()
+    );
+
+    if (
+      this.skills.length <= 0 ||
+      isPersonExist ||
+      !this.taskForm.valid ||
+      age < 18
+    ) {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
+  addPerson(event: any) {
+    event.preventDefault();
+    const name: string = this.taskForm.get(
+      'taskAssociatedPersons.personName'
+    ).value;
+    const age: number = Number(
+      this.taskForm.get('taskAssociatedPersons.personAge').value
+    );
+
+    //Validations
+    if (!this.isValidToAddPersons()) return;
+
+    this.persons.push({
+      name: name,
+      age: age,
+      skills: this.skills,
+    });
+
+    this.resetTaskAssociatedPersons();
+  }
+
+  resetTaskAssociatedPersons() {
+    this.taskForm.get('taskAssociatedPersons').reset();
+    this.skills = [];
+  }
+
+  removePerson(person: Person): void {
+    const index = this.persons.indexOf(person);
+
+    if (index >= 0) {
+      this.persons.splice(index, 1);
+    }
+  }
+
+  openSnackBar(message: string, action: string) {
+    this.snackBar.open(message, action, {
+      duration: 3000,
+    });
   }
 }
