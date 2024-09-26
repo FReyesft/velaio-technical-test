8847bd3745a2244ebbf601c601c971270bf9cb2e
freyesft <f.reyesft@gmail.com>
2024-09-26 11:04:42 -0500

    [ADD] normalize css and componetize (header and footer)

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MainComponent } from './main/main.component';
+import { AppNavbarComponent } from './shared/app-navbar/app-navbar.component';
+import { FooterComponent } from './shared/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainComponent,
+    AppNavbarComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule,
---
+++ frontend/src/app/shared/app-navbar/app-navbar.component.ts
@@ -0,0 +1,11 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './app-navbar.component.html',
+  styleUrls: ['./app-navbar.component.scss']
+})
+export class AppNavbarComponent {
+
+}
