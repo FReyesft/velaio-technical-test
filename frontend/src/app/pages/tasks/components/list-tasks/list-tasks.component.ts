890dd0a922a79838a3d8bc9aac6fc9646e1f949d
freyesft <f.reyesft@gmail.com>
2024-09-27 11:52:42 -0500

    [FIX] status from route

--- frontend/src/app/pages/tasks/components/list-tasks/list-tasks.component.ts
+++ frontend/src/app/pages/tasks/components/list-tasks/list-tasks.component.ts
@@ -1,17 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Task } from 'src/app/interfaces/task.interface';
 import { TasksService } from '../../services/tasks.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-list-tasks',
   templateUrl: './list-tasks.component.html',
   styleUrls: ['./list-tasks.component.scss'],
 })
-export class ListTasksComponent implements OnInit {
+export class ListTasksComponent implements OnInit, OnDestroy {
   public tasks: Task[] = [];
   public filteredTasks: Task[] = [];
   public filter: 'all' | 'completed' | 'pending' = 'all';
+  private routeSubscription: Subscription;
 
   constructor(
     private tasksService: TasksService,
@@ -19,9 +21,24 @@ export class ListTasksComponent implements OnInit {
     private router: Router
   ) {}
 
+  ngOnDestroy(): void {
+    this.routeSubscription.unsubscribe();
+  }
+
   ngOnInit(): void {
     this.tasks = this.tasksService.getTasks();
     this.filteredTasks = this.tasks;
+    this.routeSubscription = this.activatedRoute.queryParams.subscribe(
+      (params) => {
+        const status = params['status'];
+        if (status === 'completed' || status === 'pending') {
+          this.filter = status;
+        } else {
+          this.filter = 'all';
+        }
+        this.addFilter();
+      }
+    );
   }
 
   toggleTaskCompletion(task: Task) {
